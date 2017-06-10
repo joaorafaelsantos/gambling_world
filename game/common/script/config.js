7fc52e59871cb5864f43927ff5a3b72850f50b25
João Rafael Santos <joaorafael23@gmail.com>
2017-06-10 15:04:38 +0100

    Restructure and menu sounds

--- game/common/script/config.js
+++ game/common/script/config.js
@@ -1,4 +1,22 @@
+var soundSelect = new Audio('common/sounds/selection.wav');
+var soundStart = new Audio('common/sounds/start.ogg');
+var soundTheme = new Audio('common/sounds/theme.wav');
+
+Audio.prototype.stop = function () {
+    this.pause();
+    this.currentTime = 0;
+};
+
+var doSound;
+
+var flag = true;
 $(function () {
+    // Start theme song
+    soundTheme.addEventListener('ended', function () {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    soundTheme.play();
     // Copy the div #sectionMenu to the variale initalContent
     var initialContent = $('#sectionMenu').clone();
     // Flag of mobile detection 
@@ -19,20 +37,6 @@ $(function () {
     // Run the function on ready
     checkMobile();
 
-    // // On window resize detect the size of the window and do the mobile function
-    // $(window).on('resize', function () {
-    //     width = $(window).width();
-    //     height = $(window).height();
-    //     checkMobile();
-    //     // If the page isn't loading from mobile browser but have a width smaller or equal than 1024, it adjust to mobile
-    //     if (!isMobile && width <= 1024) {
-    //         adjustMobile()
-    //         // If the page isn't loading from mobile browser but have a width greater than 1024, it adjust to desktop
-    //     } else if (!isMobile && width > 1024) {
-    //         adjustDesktop();
-    //     }
-    // });
-
     // Function that adjust the content to mobile (the game don't work on mobile)
     function adjustMobile() {
         $('#sectionMenu').remove();
@@ -40,31 +44,197 @@ $(function () {
         $('#sectionLogo').after(content);
         $('h2').css("padding-top", "0px")
     }
-    // // Function that adjust the content to desktop
-    // function adjustDesktop() {
-    //     $('#sectionMenu').remove();
-    //     $('#sectionLogo').after(initialContent);
-    //     $('h2').css("padding-top", "125px")
-    // }
 
     // Load how to play section after click
     $('#howToPlay').click(function () {
+        soundTheme.stop();
+        soundSelect.stop();
+        soundStart.play();
         $('#sectionMenu').load('common/pages/how-to-play.html')
     });
 
     // Load game modes section after click
     $('#play').click(function () {
+        soundTheme.stop();
+        soundSelect.stop();
+        soundStart.play();
         $('#sectionMenu').load('common/pages/game-modes.html')
     });
 
     // Load credits section after click
     $('#credits').click(function () {
+        soundTheme.stop();
+        soundSelect.stop();
+        soundStart.play();
         $('#sectionMenu').load('common/pages/credits.html')
     });
 
     $('#btnBackMenu').click(function () {
+        soundTheme.stop();
+        soundSelect.stop();
+        soundStart.play();
         location.reload();
     });
+
+    $(".btnSound").on("mouseover", function () {
+        soundSelect.stop();
+        soundSelect.play();
+    });
+
+    $("a").find("i").click(function () {
+        soundStart.play();
+    })
+
+    // Game Modes
+
+    $('#vira').click(function () {
+        soundTheme.stop();
+        soundSelect.stop();
+        soundStart.play();
+        $('#sectionMenu').load('common/pages/play-vira.html');
+    });
+    $('#hotSpin').click(function () {
+        soundTheme.stop();
+        soundSelect.stop();
+        soundStart.play();
+        $('#sectionMenu').load('common/pages/play-hot-spin.html');
+    });
+    $('#bancaFrancesa').click(function () {
+        soundTheme.stop();
+        soundSelect.stop();
+        soundStart.play();
+        $('#sectionMenu').load('common/pages/play-banca-francesa.html');
+    });
+
+    // Configurations
+
+    doSound = function doSound() {
+        soundTheme.stop();
+        soundSelect.stop();
+        soundStart.play();
+    }
+
+    var players = []
+    function restoreLocalStorage() {
+        for (var i = 0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            var y = JSON.parse(localStorage.getItem(key));
+            players.push(y);
+        }
+    }
+
+    // Save on localstorage
+    function saveLocalStorage() {
+        // Check browser support
+        if (typeof (Storage) !== "undefined") {
+            // Store
+            for (var i = 0; i < players.length; i++) {
+                localStorage.setItem(i.toString(), JSON.stringify(players[i]));
+            }
+        } else {
+            console.log("Error", "Sorry, your browser does not support Web Storage...", "error");
+        }
+    }
+
+    // Vira configurations
+
+
+    $('#playVira').click(function () {
+        doSound();
+        players = [];
+
+        if (localStorage.length != 0) {
+            restoreLocalStorage();
+        }
+        // Create object players with player info
+        var player = {};
+        if (players.length == 0) {
+            player.name = $("#user").val();
+            player.money = 100;
+            player.probability = $("#sel1 option:selected").text();
+            player.timestamp = new Date().getTime() / 1000;
+            players.push(player);
+        } else {
+            var exists = false;
+            var index = 0;
+            for (var i = 0; i < players.length; i++) {
+                var tempName = $("#user").val();
+                var tempPlayer = players[i];
+                if (tempPlayer.name == tempName) {
+                    exists = true;
+                    index = i;
+                }
+            }
+            if (exists == true) {
+                players[index].probability = $("#sel1 option:selected").text();
+                players[index].timestamp = new Date().getTime() / 1000;
+                if (players[index].money < 1) {
+                    players[index].money = 1;
+                }
+            } else if (exists == false) {
+                player.name = $("#user").val();
+                player.money = 100;
+                player.probability = $("#sel1 option:selected").text();
+                player.timestamp = new Date().getTime() / 1000;
+                players.push(player);
+            }
+        }
+
+        // Save on localstorage
+        saveLocalStorage();
+
+        function reload() {
+            window.location.replace('common/games/vira/vira.html');
+        }
+        setTimeout(reload, 1000);
+    });
+
+    $("#playHotSpin").click(function () {
+        players = [];
+        if (localStorage.length != 0) {
+            restoreLocalStorage();
+        }
+        // Create object players with player info
+        var player = {};
+        if (players.length == 0) {
+            player.name = $("#user").val();
+            player.money = 100;
+            player.timestamp = new Date().getTime() / 1000;
+            players.push(player);
+        } else {
+            var exists = false;
+            var index = 0;
+            for (var i = 0; i < players.length; i++) {
+                var tempName = $("#user").val();
+                var tempPlayer = players[i];
+                if (tempPlayer.name == tempName) {
+                    exists = true;
+                    index = i;
+                }
+            }
+
+            if (exists == true) {
+                players[index].timestamp = new Date().getTime() / 1000;
+            } else if (exists == false) {
+                player.name = $("#user").val();
+                player.money = 100;
+                player.timestamp = new Date().getTime() / 1000;
+                players.push(player);
+            }
+
+        }
+        // Save on localstorage
+        saveLocalStorage();
+
+        window.location.replace('common/games/hot-spin/hot-spin.html');
+    });
+
+
+
+
+
+
+
 
 
 
